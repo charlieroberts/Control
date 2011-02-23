a2049be69b57611a1f966be59f64cb998d47924b
Alex Norman <x37v.alex@gmail.com>
2011-02-23 10:55:44 -0800

    loading the index from the assets dir now

--- Android/Control/src/com/charlieroberts/Control/Control.java
+++ Android/Control/src/com/charlieroberts/Control/Control.java
@@ -1,23 +1,22 @@
 
-    package com.charlieroberts.Control;
+package com.charlieroberts.Control;
 
-    import android.os.Bundle;
-    import com.phonegap.*;
-	import android.view.WindowManager;
-	
-    public class Control extends DroidGap
-    {
-        @Override
-        public void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            super.setIntegerProperty("loadUrlTimeoutValue", 60000);
-			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN ); 
-    		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-			WindowManager.LayoutParams.FLAG_FULLSCREEN); 
-            super.loadUrl("http://10.42.43.1:8000/www/index.html");
-			//super.loadUrl("file:///android_asset/www/index.html");
-			
-        }
-    }
-    
+import android.os.Bundle;
+import com.phonegap.*;
+import android.view.WindowManager;
+
+public class Control extends DroidGap
+{
+   @Override
+      public void onCreate(Bundle savedInstanceState)
+      {
+         super.onCreate(savedInstanceState);
+         super.setIntegerProperty("loadUrlTimeoutValue", 60000);
+         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN ); 
+         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+               WindowManager.LayoutParams.FLAG_FULLSCREEN); 
+         //super.loadUrl("http://10.42.43.1:8000/www/index.html");
+         super.loadUrl("file:///android_asset/www/index.html");
+      }
+}
+
