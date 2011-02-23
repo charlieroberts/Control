0db245b072de663334ba0b9ae0f6c9b39a105ac1
thecharlie <charlie@charlie-roberts.com>
2011-02-23 15:46:38 -0800

    modifications for merge

--- Android/Control/src/com/charlieroberts/Control/Control.java
+++ Android/Control/src/com/charlieroberts/Control/Control.java
@@ -15,7 +15,8 @@ public class Control extends DroidGap
          getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN ); 
          getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
                WindowManager.LayoutParams.FLAG_FULLSCREEN); 
-         //super.loadUrl("http://10.42.43.1:8000/www/index.html");
+         //super.loadUrl("http://192.168.1.112/~charlie/www/index.html");
+//         super.loadUrl("file:///android_asset/www/index.html");
          super.loadUrl("file:///android_asset/www/index.html");
       }
 }
