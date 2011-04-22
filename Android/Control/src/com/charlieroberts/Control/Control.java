b2ee63c7f9c5f07f85ede1c8986296fac5ae3bf4
Alex Norman <x37v.alex@gmail.com>
2011-04-22 14:46:26 -0700

    simply sending a test osc message on control startup
    just checking in my progress... the android is now sending osc, but, it
    isn't hooked into the app logic yet

--- Android/Control/src/com/charlieroberts/Control/Control.java
+++ Android/Control/src/com/charlieroberts/Control/Control.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import com.phonegap.*;
 import android.view.WindowManager;
 
+import de.sciss.net.*;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+
 public class Control extends DroidGap
 {
    @Override
@@ -17,6 +23,29 @@ public class Control extends DroidGap
 
          //super.loadUrl("http://192.168.1.6/~charlie/www/index.html");
          super.loadUrl("file:///android_asset/www/index.html");
+
+         final Object        sync = new Object();
+         final OSCClient     c;
+         final OSCBundle     bndl1, bndl2;
+         final Integer       nodeID;
+
+         try {
+            //start client, binding to any available port
+            c = OSCClient.newUsing(OSCClient.TCP);
+            c.setTarget( new InetSocketAddress( "10.0.0.3", 10001 ));
+            // open channel and (in the case of TCP) connect, then start listening for replies
+            c.start();
+            c.dumpOSC( OSCChannel.kDumpBoth, System.err );
+            c.send( new OSCMessage( "/test", new Object[] { "from Control!", new Integer( 42 )}));
+         }
+         catch( IOException e ) {
+            System.err.println("CRAP NetUtil osc sending isn't working!!!");
+
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            System.err.println( sw.toString());
+         }
+
       }
 }
 
