97a6ead8fb04c1c73f2acff2fec15dad3f5a1aeb
thecharlie <charlie@charlie-roberts.com>
2012-01-19 14:02:23 -0800

    fixes for knob widget

--- www/js/Knob.js
+++ www/js/Knob.js
@@ -1,5 +1,3 @@
-// TODO: do non-rotational knobs still work without using rotationValue??? don't think so...
-
 function Knob(ctx,props) {
 	// MUST COME BEFORE WIDGET INIT SINCE JSON DOESN'T INCLUDE WIDTH AND HEIGHT FOR KNOBS, ONLY RADIUS
 	props.width  = props.radius;
@@ -26,6 +24,7 @@ function Knob(ctx,props) {
 	this.knobBuffer = 1;
 	
 	this.lastValue = this.value;
+    this.lastAngle = 0;
 	this.usesRotation = (typeof props.usesRotation != "undefined") ? props.usesRotation : true;
 	
 	if(this.centerZero)
@@ -35,7 +34,7 @@ function Knob(ctx,props) {
 	
 	this.canvas = document.createElement('canvas');
 	$(this.canvas).addClass('widget knob');
-
+    
 	if(control.orientation == 0 || control.orientation == 180)  
 		this.height = this.width;
 	else
@@ -44,7 +43,7 @@ function Knob(ctx,props) {
 	this.canvas.width = this.width;						// DO NOT USE STYLES TO RESIZE CANVAS OBJECT
 	this.canvas.height = this.width;					// DO NOT USE STYLES TO RESIZE CANVAS OBJECT
 	this.ctx.appendChild(this.canvas);
-
+    
 	//this.canvas.style.border = "1px solid #fff";
 	this.canvas.style.top = this.y + "px";
 	this.canvas.style.left = this.x + "px";
@@ -64,7 +63,7 @@ function Knob(ctx,props) {
 			_height = (this.labelSize + 4) / control.deviceHeight;
 			_x = props.x + (props.radius / 2) - (_width / 2);
 			_y = props.y + (props.radius / 3) - _height / 2;
-
+            
             this.label = {
 				"name":  	this.name + "Label",
 				"type":  	"Label", 
@@ -74,19 +73,20 @@ function Knob(ctx,props) {
 				"size" : 	props.labelSize || 12,
  				"backgroundColor": "rgba(127, 127, 127, .75)",				
 			};
-                        
+            
             var _w = control.makeWidget(this.label);
             control.widgets.push(_w);
 	        if(!control.isAddingConstants)
 	            control.addWidget(_w, control.currentPage); // PROBLEM
 	        else
 	            control.addConstantWidget(_w); // PROBLEM
-  
+            
             this.label = _w;
         }
     }
-
+    
     this.setValue(this.value, false);
+	this.lastRotationValue = .05;
     
     return this;
 }
@@ -96,10 +96,11 @@ Knob.prototype = new Widget();
 
 Knob.prototype.draw = function() {
     this.canvasCtx.clearRect(0, 0, this.width,this.height);
-    this.canvasCtx.fillStyle = this.backgroundColor;
     this.canvasCtx.strokeStyle = this.strokeColor;
-    this.canvasCtx.lineWidth = 1.5;	
-
+    this.canvasCtx.lineWidth = 1.5;
+	
+	this.canvasCtx.fillStyle = this.backgroundColor; // draw background of widget first
+    
     var angle0 = Math.PI * .6;
     var angle1 = Math.PI * .4;
     
@@ -108,10 +109,11 @@ Knob.prototype.draw = function() {
     this.canvasCtx.arc(this.radius, this.radius, (this.radius - this.knobBuffer) * 0.3 , angle1, angle0, true);		
     this.canvasCtx.closePath();
     this.canvasCtx.fill();
-
+    
     this.canvasCtx.stroke();
     
-    this.canvasCtx.fillStyle = this.fillColor;
+    this.canvasCtx.fillStyle = this.fillColor;	// now draw foreground...
+	
     if(this.centerZero) {
         var angle3 = Math.PI * 1.5;
         var angle4 = Math.PI * (1.5 + (.9 * (-1 + (this.rotationValue * 2))));
@@ -121,21 +123,22 @@ Knob.prototype.draw = function() {
         this.canvasCtx.arc(this.radius , this.radius, (this.radius - this.knobBuffer) * 0.3,  angle4, angle3, (this.rotationValue > .5));
         this.canvasCtx.closePath();
         /*if(this.rotationValue == .5) { // draw circle if centered?
-            this.canvasCtx.beginPath();
-            this.canvasCtx.arc(this.radius , this.radius, (this.radius -  this.knobBuffer) * .3, 0, Math.PI*2, true); 
-            this.canvasCtx.closePath();
-        }*/
+         this.canvasCtx.beginPath();
+         this.canvasCtx.arc(this.radius , this.radius, (this.radius -  this.knobBuffer) * .3, 0, Math.PI*2, true); 
+         this.canvasCtx.closePath();
+         }*/
         this.canvasCtx.fill();
     } else {
         if(!this.isInverted)   
-            var angle2 = Math.PI * (0.6 + (1.8 * this.rotationValue));
+            var angle2 = 1.6 + (this.rotationValue * 2) * Math.PI; //Math.PI * (0.6 + (1.8 * this.rotationValue));
         else
             var angle2 = Math.PI * (0.4 - (1.8 * this.rotationValue));
         
         this.canvasCtx.beginPath();
+		//context.arc(centerX, centerY, radius, startingAngle, endingAngle, antiClockwise);
         if(!this.isInverted) {
             this.canvasCtx.arc(this.radius, this.radius, this.radius - this.knobBuffer, angle0, angle2, false);
-            this.canvasCtx.arc(this.radius, this.radius, (this.radius - this.knobBuffer) * 0.3, angle2, angle0, true);
+            this.canvasCtx.arc(this.radius, this.radius, (this.radius - this.knobBuffer) * .3, angle2, angle0, true);
         } else {
             this.canvasCtx.arc(this.radius, this.radius, this.radius - this.knobBuffer, angle1, angle2 ,true);
             this.canvasCtx.arc(this.radius, this.radius, (this.radius - this.knobBuffer) * 0.3, angle2, angle1, false);
@@ -176,6 +179,7 @@ Knob.prototype.event = function(event) {
     touch = event.changedTouches.item(0);
     
     if(event.type == "touchstart" && this.hitTest(touch.pageX, touch.pageY)) { // if touch starts over this widget
+		this.newTouch = true;
         this.activeTouches.push(touch.identifier);
         this.lastPosition = touch.pageY;
         this.changeValue(touch.pageY, touch.pageX);
@@ -183,9 +187,11 @@ Knob.prototype.event = function(event) {
     } else {
         for(i in this.activeTouches) {
             if(event.type == "touchmove" && touch.identifier == this.activeTouches[i]) {		// if moved touch ID is in the list of active touches
+				this.newTouch = false;
                 this.changeValue(touch.pageY, touch.pageX);
                 eval(this.ontouchmove);
             }else if(event.type == "touchend" && touch.identifier == this.activeTouches[i]) {	// if ended touch ID is in the list of active touches
+				this.newTouch = false;
                 this.activeTouches.splice(i,1);	// remove touch ID from array
                 this.lastPosition = -1;
                 eval(this.ontouchend);
@@ -216,25 +222,36 @@ Knob.prototype.setValue = function(newValue) {
 		this.output();
     
     this.draw();
-
+    
 };
 
 Knob.prototype.changeValue = function(yinput, xinput) {
+	// TODO: accommodate !usesRotation and centeredRotation.
     this.lastValue = this.value;
     
     if(!this.usesRotation) {
-        if (this.lastPosition != -1) this.rotationValue -= (yinput - this.lastPosition) / (this.width / (1 + 1)); // (2 + 2*this.centerZero) is magic number - fix it!
+        if (this.lastPosition != -1) { 
+            this.rotationValue -= (yinput - this.lastPosition) / (this.width / 2);
+        }
     }else{
         var xdiff = (this.x + (this.width / 2)) - xinput;
         var ydiff = (this.y + (this.height / 2)) - yinput;
         var angle = 180 + Math.atan2(ydiff, xdiff) * (180 / Math.PI);
-        this.rotationValue = ((angle + 270) % 360) / 360;
+        this.rotationValue =  ((angle + 270) % 360) / 360;
     }
+    console.log(this.rotationValue);
+    if (this.rotationValue > .95) this.rotationValue = .95;
+    if (this.rotationValue < .05) this.rotationValue = .05;
     
-    if (this.rotationValue > 1) this.rotationValue = 1;
-    if (this.rotationValue < 0) this.rotationValue = 0;
-    
+	if(this.lastRotationValue == .95 && this.rotationValue <= .5 && !this.newTouch) {
+		this.rotationValue = .95;
+		return;
+	}else if(this.lastRotationValue == .05 && this.rotationValue >= .5 && !this.newTouch) {
+		this.rotationValue = .05;
+		return;
+	}
     //console.log("rotationValue = " + this.rotationValue);
+	this.lastRotationValue = this.rotationValue;
     this.lastPosition = yinput;
     
     var range  = this.max - this.min;
